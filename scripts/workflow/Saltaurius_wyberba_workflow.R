ec703c38316ea70329334cde3bb33a8cec4d0432
Roozbeh Valavi <valavi.r@gmail.com>
2020-06-26 10:52:18 +1000

    Saltaurius wyberba new workflow

--- scripts/workflow/Saltaurius_wyberba_workflow.R
+++ scripts/workflow/Saltaurius_wyberba_workflow.R
@@ -64,17 +64,14 @@ library(bushfireSOS)
 
 ## Presence background data
 
-tm <- Sys.time()
 spp_data <- bushfireSOS::load_pres_bg_data_AUS(species = species,
                                                region = c("VIC", "NSW", "QLD", "SA", "NT", "WA", "TAS"),
                                                save.map = FALSE,
                                                map.directory = "outputs/data_outputs",
-                                               email = "rvalavi@student.unimelb.edu.au",
+                                               email = "",
                                                file.vic = "../../bushfireResponse_data/spp_data_raw/VIC sensitive species data/FAUNA_requested_spp_ALL.gdb")
-Sys.time() - tm
 
 region <- bushfireSOS::species_data_get_state_character(spp_data$data)
-print(region)
 
 ## Presence absence data
 
@@ -109,6 +106,12 @@ spp_data <- bushfireSOS::background_points(species = species,
                                            bias_layer = "../../bushfireResponse_data/spatial_layers/aus_road_distance_250_aa.tif",
                                            sample_min = 1000)
 
+## Check that there are >= 20 presences (1s) and an appropriate number of
+## background points (1000 * number of states with data for target group,
+## or 10,000 for random)
+
+table(spp_data$data$Value)
+
 #######################
 ### Data Extraction ###
 #######################
@@ -126,8 +129,6 @@ saveRDS(spp_data,
 
 # Do we have >=20 presence records?
 # Y/N
-
-nrow(spp_data$data[spp_data$data$Value == 1, ])
 
 # Can we fit an SDM for this species?
 # Y/N 
@@ -180,10 +181,12 @@ saveRDS(model,
 
 # Perform appropriate model checking
 # Ensure features is set identical to that of the above full model
+# If Boyce Index returns NAs then re-run the cross-validation with
+#  one fewer fold i.e. 5 > 4 > 3 > 2 > 1
 
 model_eval <- bushfireSOS::cross_validate(spp_data = spp_data,
                                           type = "po",
-                                          k = 4,
+                                          k = 5,
                                           parallel = FALSE,
                                           features = "default")
 
@@ -200,8 +203,7 @@ saveRDS(model_eval,
 prediction <- bushfireSOS::model_prediction(model = model,
                                             env_data = env_data,
                                             mask = "../../bushfireResponse_data/spatial_layers/NIAFED_v20200428",
-                                            parallel = TRUE, 
-                                            ncors = 6)
+                                            parallel = FALSE)
 
 raster::writeRaster(prediction,
                     sprintf("../../bushfireResponse_data/outputs/predictions/predictions_%s.tif",
