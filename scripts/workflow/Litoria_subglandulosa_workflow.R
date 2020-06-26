79d307d94c4b2d165460783a8218678df2547456
Roozbeh Valavi <valavi.r@gmail.com>
2020-06-26 23:38:35 +1000

    Litoria subglandulosa new workflow

--- scripts/workflow/Litoria_subglandulosa_workflow.R
+++ scripts/workflow/Litoria_subglandulosa_workflow.R
@@ -36,11 +36,11 @@
 ## Region:                            # NSW, QLD
 ## Analyst:                           # Roozbeh Valavi
 ## Reviewer:                          # Darren Southwell @dsouthwellw
-## SDM Required: Y/N                  # NA
+## SDM Required: Y/N                  # YES
 ## Used existing SDM: Y/N             # NO
-## Built SDM: Y/N                     # NO
-## Data available: PO/PA              # NO
-## Type of SDM: PresBG/PresAbs/Hybrid # NA
+## Built SDM: Y/N                     # YES
+## Data available: PO/PA              # PO
+## Type of SDM: PresBG/PresAbs/Hybrid # PO
 ## Date completed:                    # 24-06-2020
 
 species <- "Litoria subglandulosa"
@@ -64,16 +64,15 @@ library(bushfireSOS)
 
 ## Presence background data
 
-tm <- Sys.time()
 spp_data <- bushfireSOS::load_pres_bg_data_AUS(species = species,
                                                region = c("VIC", "NSW", "QLD", "SA", "NT", "WA", "TAS"),
                                                save.map = FALSE,
                                                map.directory = "outputs/data_outputs",
                                                email = "rvalavi@student.unimelb.edu.au",
                                                file.vic = "../../bushfireResponse_data/spp_data_raw/VIC sensitive species data/FAUNA_requested_spp_ALL.gdb")
-Sys.time() - tm
 
 region <- bushfireSOS::species_data_get_state_character(spp_data$data)
+print(region)
 
 ## Presence absence data
 
@@ -108,6 +107,12 @@ spp_data <- bushfireSOS::background_points(species = species,
                                            bias_layer = "../../bushfireResponse_data/spatial_layers/aus_road_distance_250_aa.tif",
                                            sample_min = 1000)
 
+## Check that there are >= 20 presences (1s) and an appropriate number of
+## background points (1000 * number of states with data for target group,
+## or 10,000 for random)
+
+table(spp_data$data$Value)
+
 #######################
 ### Data Extraction ###
 #######################
@@ -125,8 +130,6 @@ saveRDS(spp_data,
 
 # Do we have >=20 presence records?
 # Y/N
-
-nrow(spp_data$data[spp_data$data$Value == 1, ])
 
 # Can we fit an SDM for this species?
 # Y/N 
@@ -179,6 +182,8 @@ saveRDS(model,
 
 # Perform appropriate model checking
 # Ensure features is set identical to that of the above full model
+# If Boyce Index returns NAs then re-run the cross-validation with
+#  one fewer fold i.e. 5 > 4 > 3 > 2 > 1
 
 model_eval <- bushfireSOS::cross_validate(spp_data = spp_data,
                                           type = "po",
@@ -199,8 +204,8 @@ saveRDS(model_eval,
 prediction <- bushfireSOS::model_prediction(model = model,
                                             env_data = env_data,
                                             mask = "../../bushfireResponse_data/spatial_layers/NIAFED_v20200428",
-                                            parallel = TRUE, 
-                                            ncors = 6)
+                                            parallel = TRUE,
+                                            ncors = 4)
 
 raster::writeRaster(prediction,
                     sprintf("../../bushfireResponse_data/outputs/predictions/predictions_%s.tif",
