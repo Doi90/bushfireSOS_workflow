ea0a1bb4e17fd4bbda80ed9b7099c78494929285
Roozbeh Valavi <valavi.r@gmail.com>
2020-07-01 12:04:58 +1000

    Pseudemoia rawlinsoni new workflow

--- scripts/workflow/Pseudemoia_rawlinsoni_workflow.R
+++ scripts/workflow/Pseudemoia_rawlinsoni_workflow.R
@@ -41,7 +41,9 @@
 ## Built SDM: Y/N                     # YES
 ## Data available: PO/PA              # PO
 ## Type of SDM: PresBG/PresAbs/Hybrid # PresBG
-## Date completed:                    # 27-06-2020
+## Date completed:                    # 30-06-2020
+## Number of occurrence               # 82
+## Comment                            # random BG points
 
 species <- "Pseudemoia rawlinsoni"
 
@@ -91,7 +93,7 @@ nrow(spp_data$data)
 
 # Load appropriate environmental raster data
 
-env_data <- bushfireSOS::load_env_data(stack_file = "../../bushfireResponse_data/spatial_layers/bushfire_terre_layers_250_AA.tif",
+env_data <- bushfireSOS::load_env_data(stack_file = "../../bushfireResponse_data/spatial_layers/raster_tiles",
                                        region = region)
 
 #########################
@@ -207,10 +209,12 @@ prediction <- bushfireSOS::model_prediction(model = model,
                                             mask = "../../bushfireResponse_data/spatial_layers/NIAFED_v20200428",
                                             parallel = TRUE,
                                             ncors = 4)
+mapview::mapview(prediction)
 
 raster::writeRaster(prediction,
                     sprintf("../../bushfireResponse_data/outputs/predictions/predictions_%s.tif",
-                            gsub(" ", "_", species)))
+                            gsub(" ", "_", species)),
+                    overwrite = TRUE)
 
 #################
 ### Meta Data ###
